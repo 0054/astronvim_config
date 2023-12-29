cd6ed27e0f33a32c78b673cd710c775550f4e778
pushkin <e.pushkin@traceair.net>
2023-12-30 02:13:37 +0400

    add colorschemes and update config

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,29 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "neanias/everforest-nvim",
+    config = function()
+      require("everforest").setup({
+        -- background = "hard",
+        ---How much of the background should be transparent. 2 will have more UI
+        ---components be transparent (e.g. status line background)
+        -- transparent_background_level = 1,
+      })
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      require('nightfox').setup({
+        options = {
+          styles = {
+            transparent = false,     -- Disable setting background
+            terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+          }
+        }
+      })
+    end,
+
+  }
 }
